3d03e931936a245c1999163917c1c6e554179797
menegass0 <victormenegasso@unisantos.br>
2023-02-13 15:15:05 -0300

    #

---
+++ calendar/script.js
@@ -0,0 +1,48 @@
+const currentDate = document.querySelector(".current-date"),
+daysTag = document.querySelector(".days"),
+previusNextIcon = document.querySelectorAll(".icons span");
+
+let date = new Date();
+currYear = date.getFullYear();
+currMonth = date.getMonth();
+
+const months =["January","February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"];
+
+
+const renderCalendar = () =>{
+    let lastDateofMonth = new Date(currYear, currMonth +1 , 0).getDate(),//getting last date of month
+    firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),//getting first day of month
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay()
+    lastDateodLastMonth = new Date(currYear, currMonth, 0).getDate();//getting last of previus month
+    let liTag = "";
+
+    for(let i = firstDayofMonth;i > 0; i--){
+        liTag += `<li class="inactive">${lastDateodLastMonth - i + 1}</li>`;
+    }
+
+    for(let i= 1; i<=lastDateofMonth;i++){
+        let isToday = i=== date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
+
+        liTag += `<li class="${isToday}">${i}</li>`;
+    }
+    for(let i = lastDayofMonth;i < 6; i++){
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+    }
+    currentDate.innerHTML =`${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+}
+renderCalendar();
+
+previusNextIcon.forEach(icon =>{
+    icon.addEventListener("click", ()=>{
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth +1;
+        if(currMonth < 0 || currMonth > 11){
+            date = new Date(currYear, currMonth);
+            currYear =  date.getFullYear();
+            currMonth = date.getMonth();
+        }else{
+            date = new Date();
+        }
+        renderCalendar();
+    })
+})
